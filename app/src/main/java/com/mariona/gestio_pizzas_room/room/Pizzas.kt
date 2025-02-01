925089fb8618b5e49f1aa4849d68badb98dc8b37
Marionaaa22 <mariona.claros@mataro.epiaedu.cat>
2025-02-01 23:52:16 +0100

    errors corregido su puta madre

--- app/src/main/java/com/mariona/gestio_pizzas_room/room/Pizzas.kt
+++ app/src/main/java/com/mariona/gestio_pizzas_room/room/Pizzas.kt
@@ -1,11 +1,10 @@
 package com.mariona.gestio_pizzas_room.room
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 @Entity(tableName = "Pizza") // Nombre de la tabla
 data class Pizzas(
     @PrimaryKey val referencia: String, // La clave primaria
@@ -13,4 +12,4 @@ data class Pizzas(
     val tipo: String,
     val preu: Double,
     val preuIVA: Double
-) : Parcelable
+)
